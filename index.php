2943e828c2d2bee50e148caa5a8a831fac1bc99b
Nathan Edwards <soundofhonesty@gmail.com>
2012-01-19 20:58:27 +0000

    abstracts out tileset to level class which renders tileset

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ error_reporting(E_ALL);
 
 require 'tileEngine/tileset.php';
 require 'tileEngine/tile.php';
+require 'tileEngine/level.php';
 
-$tileset = new Tileset("json/maps/level1.json");
+//$tileset = new Tileset("json/maps/level1.json");
 
-
+$level = new level();
+$level->add_tileset(new Tileset("json/maps/level1.json"));
 header('content-type: image/png');
 
-echo $tileset->generate_image();
+echo $level->render_level();
--- tileEngine/level.php
+++ tileEngine/level.php
@@ -9,13 +9,24 @@ class Level
 		return $this->tilesets;
 	}
 	
-	public function add_tileset($tileset)
+	public function add_tileset(Tileset $tileset)
 	{
 		$this->tilesets[] = $tileset;
 	}
 	
 	public function render_level()
 	{
+		$tilesets  = $this->get_tilesets();
+		$size = $tilesets[0]->get_tiles()->Size;
 		
+		$image = imagecreate($size, $size);
+		
+		foreach($tilesets as $tileset)
+		{
+			$tile = $tileset->generate_image();
+			imagecopy($image, $tile, 0, 0, 0, 0, $size, $size);
+		}
+		
+		return imagepng($image);
 	}
 }
